684739c856d9fa9b5da5f7b3ea98b66fec8f981f
Kamil <kamil.trybel.wsb@gmail.com>
2023-06-20 19:39:20 +0200

    Add Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
+    <div className="formContainer">
+    <div className="formWrapper">
+        <form >
+            <span className="logo">React Chat</span>
+            <span className="title">Register</span>
+            <input type="text" placeholder='display name' />
+            <input type="email" placeholder='email' />
+            <input type="password" placeholder='password' />
+            <input type="file" />
+            <button>Sign up</button>
+            <p>You have an account? Login</p>
+        </form>
+    </div>
+</div>  
+)
 }
 
 export default Register
