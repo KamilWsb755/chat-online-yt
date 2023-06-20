ab5a876d9851cbae836759d5e25f9ae2d2cb4e02
Kamil <kamil.trybel.wsb@gmail.com>
2023-06-20 20:07:34 +0200

    Add basic home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Home from './pages/Home';
 import './style.scss'
 
 function App() {
   return (
-    <Login />
+    <Home />
   );
 }
 
