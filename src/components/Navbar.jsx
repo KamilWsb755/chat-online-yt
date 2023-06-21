3663bfce1be7bff98e0c9e99ba39090ea2c1304e
Kamil <kamil.trybel.wsb@gmail.com>
2023-06-21 20:03:25 +0200

    Add navbar/sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+    <span className="logo">React Chat</span>
+    <div className="user">
+        <img src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg" alt="woman" />
+        <span>John</span>
+        <button>Logout</button>
+    </div>
+</div>
   )
 }
 
