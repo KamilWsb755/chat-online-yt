994490fa53b5163351728abf08403b0b7b269101
Kamil <kamil.trybel.wsb@gmail.com>
2023-06-21 20:05:49 +0200

    Add search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className="search">
+    <div className="searchForm">
+        <input type="text" placeholder='find a user' />
+    </div>
+    <div className="userChat">
+        <img src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg" alt="woman" />
+        <div className="userChatInfo">
+            <span>Jane</span>
+        </div>
+    </div>
+</div>
   )
 }
 
