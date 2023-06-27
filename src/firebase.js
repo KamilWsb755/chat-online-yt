ed60ee0353d906bc042d8822244e49db6a2f74c0
Kamil <kamil.trybel.wsb@gmail.com>
2023-06-27 17:31:06 +0200

    Basic firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA3I_rpJ8tcqQ32VYPL_oCFPzA9QtMAgwU",
+    authDomain: "teb-yt.firebaseapp.com",
+    projectId: "teb-yt",
+    storageBucket: "teb-yt.appspot.com",
+    messagingSenderId: "484899314774",
+    appId: "1:484899314774:web:3f7e0606a4d5412e912ff7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
