7000808ca24f27846546d6c2dc096e45b7f3803c
Kamil <kamil.trybel.wsb@gmail.com>
2023-06-27 17:14:07 +0200

    Add styles to message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>Message</div>
+    <div className="message">
+    <div className="messageInfo">
+        <img
+            src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg"
+            alt="woman"
+        />
+        <span>just now</span>
+    </div>
+    <div className="messageContent">
+        <p>Test</p>
+        <img src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg" alt="woman" />
+    </div>
+</div>
   )
 }
 
