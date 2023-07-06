d417cb9ca2e0d4b14acf71f20c5ad79474e6c7a9
Kamil <kamil.trybel.wsb@gmail.com>
2023-07-06 18:01:53 +0200

    add react context for login

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import './App.css';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './style.scss'
 
 function App() {
   return (
-    <Register />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={<Home />}
+          />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
