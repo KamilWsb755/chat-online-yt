bf3f9882a0c788f151a511d0afa142027a758eed
Kamil <kamil.trybel.wsb@gmail.com>
2023-07-08 15:23:54 +0200

    Add chat name handler

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,12 +2,14 @@ import { db } from '../firebase';
 import { doc, onSnapshot } from "firebase/firestore";
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from '../context/ChatContext';
 
 
 const Chats = () => {
     const [chats, setChats] = useState([]);
 
     const { currentUser } = useContext(AuthContext);
+    const {dispatch} = useContext(ChatContext);
 
     useEffect(() => {
         const getChats = () => {
@@ -23,10 +25,17 @@ const Chats = () => {
         currentUser.uid && getChats();
     }, [currentUser.uid]);
 
+    const handleSelect = (u)=>{
+        dispatch({type:"CHANGE_USER", payload:u})
+    }
+
     return (
         <div className="chats">
             {Object.entries(chats)?.map((chat) => (
-                <div className="userChat" key={chat[0]}>
+                <div className="userChat" 
+                key={chat[0]}
+                onClick={()=>handleSelect(chat[1].userInfo)}
+                >
                     <img src={chat[1].userInfo.photoURL} alt="" />
                     <div className="userChatInfo">
                         <span>{chat[1].userInfo.displayName}</span>
