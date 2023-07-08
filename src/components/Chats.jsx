ad7cf9255efbcd838fde7ee31b01fc14a2754df1
Kamil <kamil.trybel.wsb@gmail.com>
2023-07-08 14:58:05 +0200

    Add chats feature

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import { db } from '../firebase';
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 
 const Chats = () => {
-  return (
-    <div className='chats'>
-    <div className="userChat">
-        <img src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg" alt="woman" />
-        <div className="userChatInfo">
-            <span>Jane</span>
-            <p>Hello</p>
+    const [chats, setChats] = useState([]);
+
+    const { currentUser } = useContext(AuthContext);
+
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, "usersChats", currentUser.uid), (doc) => {
+                setChats(doc.data());
+            });
+
+            return () => {
+                unsub();
+            };
+        };
+
+        currentUser.uid && getChats();
+    }, [currentUser.uid]);
+
+    return (
+        <div className="chats">
+            {Object.entries(chats)?.map((chat) => (
+                <div className="userChat" key={chat[0]}>
+                    <img src={chat[1].userInfo.photoURL} alt="" />
+                    <div className="userChatInfo">
+                        <span>{chat[1].userInfo.displayName}</span>
+                        <p>{chat[1].lastMessage?.text}</p>
+                    </div>
+                </div>
+            ))}
         </div>
-    </div>
-    <div className="userChat">
-        <img src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg" alt="woman" />
-        <div className="userChatInfo">
-            <span>Jane</span>
-            <p>Hello</p>
-        </div>
-    </div>
-    <div className="userChat">
-        <img src="https://cdn.pixabay.com/photo/2016/12/04/02/01/celtic-woman-1880944__340.jpg" alt="woman" />
-        <div className="userChatInfo">
-            <span>Jane</span>
-            <p>Hello</p>
-        </div>
-    </div>
-</div>
-  )
+    )
 }
 
 export default Chats
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -49,13 +49,11 @@ const Search = () => {
                 : user.uid + currentUser.uid;
         try {
             const res = await getDoc(doc(db, "chats", combinedId));
-
             if (!res.exists()) {
                 //create a chat in chats collection
                 await setDoc(doc(db, "chats", combinedId), { messages: [] });
-
                 //create user chats
-                await updateDoc(doc(db, "userChats", currentUser.uid), {
+                await updateDoc(doc(db, "usersChats", currentUser.uid), {
                     [combinedId + ".userInfo"]: {
                         uid: user.uid,
                         displayName: user.displayName,
@@ -64,7 +62,7 @@ const Search = () => {
                     [combinedId + ".date"]: serverTimestamp(),
                 });
 
-                await updateDoc(doc(db, "userChats", user.uid), {
+                await updateDoc(doc(db, "usersChats", user.uid), {
                     [combinedId + ".userInfo"]: {
                         uid: currentUser.uid,
                         displayName: currentUser.displayName,
